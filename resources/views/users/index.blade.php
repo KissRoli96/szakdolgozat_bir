0ebd86d727fd3e368345ed79bde03653a3dfb92c
KissRoland <kissrolandkristof@gmail.com>
2021-04-25 13:53:07 +0200

    introduce layout

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,41 +1,25 @@
-<?php
+@extends('layout')
 
-?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Index of Users</title>
-</head>
-<body>
-<h1>
-    Userek
-</h1>
-<a href="/user/insert">Új felhasználó létrehozása</a>
-<hr>
+@section('content')
+    <h1>
+        Userek
+    </h1>
+    <a class="btn btn-info" href="/user/insert">Új felhasználó létrehozása</a>
+    <hr>
 
-<table>
+    <table class="table table-striped">
 
-    @foreach($users as $user)
-        <tr>
-            <td>
-                {{$user->name}}
-            </td>
-            <td>
-                <a href="/user/view/{{$user->id_user}}">Profil</a>
-            </td>
-        </tr>
+        @foreach($users as $user)
+            <tr>
+                <td>
+                    {{$user->name}}
+                </td>
+                <td>
+                    <a href="/user/view/{{$user->id_user}}">Profil</a>
+                </td>
+            </tr>
 
-    @endforeach
+        @endforeach
 
-</table>
-
-
-
-
-
-</body>
-</html>
+    </table>
+@endsection
