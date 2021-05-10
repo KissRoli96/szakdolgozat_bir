8738082b1b1240fb22c5f3f9ef587dc995ec312d
KissRoland <kissrolandkristof@gmail.com>
2021-05-10 22:16:04 +0200

    add the CourseDivision2019Tavasz

--- app/Models/CourseDivision2019Tavasz.php
+++ app/Models/CourseDivision2019Tavasz.php
@@ -1,1 +1,30 @@
 <?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CourseDivision2019Tavasz extends Model
+{
+    protected $table = "course_division_2019_tavasz";
+
+    protected $primaryKey = "cd_id";
+
+    protected $fillable = [
+        'cd_id',
+        'unique_name',
+        'practice_id',
+        'whose',
+        'headcount',
+        'teacher',
+        'teacher_percentage',
+        'confirm',
+        'exam',
+        'comment',
+        'deleted',
+        'forced',
+        'open',
+        'dephelper',
+    ];
+
+}
