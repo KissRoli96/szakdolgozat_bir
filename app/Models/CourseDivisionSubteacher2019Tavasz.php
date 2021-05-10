e8fd49402813738eb899e9384631a0da0b2bd0c1
KissRoland <kissrolandkristof@gmail.com>
2021-05-10 22:30:29 +0200

    add the CourseDivisionSubteacher2019Tavasz model

---
+++ app/Models/CourseDivisionSubteacher2019Tavasz.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CourseDivisionSubteacher2019Tavasz extends Model
+{
+    protected $table = "course_division_subteacher_2019_tavasz";
+
+    protected $primaryKey = "subteacher_id";
+
+    protected $fillable = [
+        'subteacher_id',
+        'course_id',
+        'teacher_id',
+        'percentage',
+        'active',
+    ];
+
+}
