8fc6c9afe921cd47a93e3b0fd9c2d31aa2c03ddf
KissRoland <kissrolandkristof@gmail.com>
2021-05-10 21:53:19 +0200

    add the DepartmentLeaders modell

---
+++ app/Models/DepartmentLeaders.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DepartmentLeaders extends Model
+{
+    protected $table = "department_leaders";
+
+    protected $primaryKey = "leader_id";
+
+    protected $fillable = [
+                'leader_id',
+                'department',
+                'leader',
+                'active',
+    ];
+ 
+}
