527e77e0446b6751d4fd9acd6d0e096acbc8dd5b
KissRoland <kissrolandkristof@gmail.com>
2021-05-10 21:01:33 +0200

    add the PersonalClaim model

---
+++ app/Models/PersonalClaim.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PersonalClaim extends Model
+{
+    protected $table = "personal_claim";
+
+    protected $primaryKey = "id";
+
+    protected  $fillable = [
+        'id',
+        'user',
+        'day',
+        'day_comment',
+        'max_lesson_con',
+        'max_lesson_day',
+        'friday',
+        'saturday',
+        'corr_comment',
+        'max_lessons',
+        'lessons_comment',
+    ];
+}
