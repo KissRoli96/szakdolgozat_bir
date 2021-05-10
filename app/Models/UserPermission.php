6f8fb8ebb8166cf9de0fda264e98e36faccf6a3f
KissRoland <kissrolandkristof@gmail.com>
2021-05-10 21:13:44 +0200

    add the UserPermission model

---
+++ app/Models/UserPermission.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserPermission extends Model
+{
+    protected $table = "user_permission";
+
+    protected $primaryKey = "id_user_permission";
+
+    protected $fillable = [
+        'id_user_permission',
+        'id_user',
+        'permission',
+        'valid',
+    ];
+}
